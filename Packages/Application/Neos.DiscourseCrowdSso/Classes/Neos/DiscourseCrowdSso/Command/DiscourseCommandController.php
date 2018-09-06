f35c1eb18494ef417cca581e7292fbe8fa7b0c48
bwaidelich <b.waidelich@wwwision.de>
2018-09-06 13:28:06 +0200

    TASK: Add minimal CLI command to test discourse API access

---
+++ Packages/Application/Neos.DiscourseCrowdSso/Classes/Neos/DiscourseCrowdSso/Command/DiscourseCommandController.php
@@ -0,0 +1,25 @@
+<?php
+namespace Neos\DiscourseCrowdSso\Command;
+
+use Neos\DiscourseCrowdSso\DiscourseService;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Cli\CommandController;
+
+class DiscourseCommandController extends CommandController
+{
+
+    /**
+     * @Flow\Inject
+     * @var DiscourseService
+     */
+    protected $discourseService;
+
+    /**
+     * A simple command to test basic access to the Discourse API
+     */
+    public function testAccessCommand()
+    {
+        $this->discourseService->hasUserWithEmail('hello@neos.io');
+        $this->outputLine('<success>Success!</success>');
+    }
+}
